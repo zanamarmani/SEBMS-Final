5b882e6e1929a19b917581b4d479d4acf13179e5
Zeeshan Hussain <zanam786armani@gmail.com>
2024-10-01 12:13:24 -0700

    applied gmail

--- SEBMS/settings.py
+++ SEBMS/settings.py
@@ -130,3 +130,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
 SESSION_COOKIE_SECURE = False  # Set this to True in production (HTTPS)
 CSRF_COOKIE_SECURE = False  # Set this to True in production (HTTPS)
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'your-email@gmail.com'
+EMAIL_HOST_PASSWORD = 'your-app-specific-password'  # Use the app password here
