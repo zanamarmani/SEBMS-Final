d3bc6e6d226f75086c3eeda04b696ef34c4a2215
Zeeshan Hussain <zanam786armani@gmail.com>
2024-10-01 12:39:33 -0700

    try to apply online payment method

--- consumer/urls.py
+++ consumer/urls.py
@@ -5,4 +5,7 @@ app_name = 'consumer'
 urlpatterns = [
     path('', views.consumer_home, name='consumer_home'),
     path('payment_gateway/', views.payment_gateway, name='payment_gateway'),
+     path('jazzcash_payment/<int:bill_id>/', views.jazzcash_payment, name='jazzcash_payment'),
+    path('payment_success/', views.payment_success, name='payment_success'),
+    path('payment_failed/', views.payment_failed, name='payment_failed'),
 ]
