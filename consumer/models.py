26684bfd857eeb5fbe4aaa886e73eaa80a2a76da
Zeeshan Hussain <zanam786armani@gmail.com>
2024-10-01 21:02:22 -0700

    tariff assign from sdo model for consumer

--- consumer/models.py
+++ consumer/models.py
@@ -1,25 +1,18 @@
 # consumer/models.py
 from django.db import models
 from users.models import User
-
+from SDO.models import Tariff
 class Consumer(models.Model):
-    # Choices for tariff types
-    TARIFF_CHOICES = [
-        ('domestic', 'Domestic'),
-        ('commercial', 'Commercial'),
-        ('industrial', 'Industrial'),
-    ]
-
     # Fields
     user = models.OneToOneField(User, null=True,on_delete=models.CASCADE,related_name='consumer')  # Linking Consumer to User model
     name = models.CharField(max_length=100)  # Consumer's name
     consumer_number = models.CharField(max_length=100, unique=True)  # Unique consumer number
     meter_number = models.CharField(max_length=100, unique=True)  # Unique meter number
     area_number = models.CharField(max_length=100)  # Area number associated with the consumer
-    tariff = models.CharField(max_length=10, choices=TARIFF_CHOICES)  # Tariff type
+    tariff = models.OneToOneField(Tariff, on_delete=models.CASCADE,null=True) # Tariff type
     approved = models.BooleanField(default=False)  # Approval status for the consumer
 
     # String representation of the Consumer instance
     def __str__(self):
-        return self.user.username if self.user else self.name
+        return self.user.email if self.user else self.name
 
